0d827cd963f486094b451905fa6cd21b5defcd8a
jdhl27 <independientemedellin.27.03@gmail.com>
2024-02-08 22:11:54 -0500

    Add context provider to user

---
+++ src/context/user/UserProvider.js
@@ -0,0 +1,41 @@
+import React, {useContext, useReducer} from 'react';
+
+import UserReducer from './UserReducer';
+import UserContext from './UserContext';
+import {SET_USER_DATA, UPDATE_CREDIT} from '../types';
+
+const UserProvider = ({children}) => {
+  const [state, dispatch] = useReducer(UserReducer, {
+    credit: 3000,
+  });
+
+  const setUserData = userData => {
+    dispatch({type: SET_USER_DATA, payload: userData});
+  };
+
+  const updateCredit = newCredit => {
+    dispatch({type: UPDATE_CREDIT, payload: newCredit});
+  };
+
+  return (
+    <UserContext.Provider
+      value={{
+        user: state,
+        credit: state.credit,
+        setUserData,
+        updateCredit,
+      }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+const useUser = () => {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error('useUser debe ser utilizado dentro de un UserProvider');
+  }
+  return context;
+};
+
+export {UserProvider, useUser};
