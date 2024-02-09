3e67242025948fd33598898f9f97a2f8c48b8e2e
jdhl27 <independientemedellin.27.03@gmail.com>
2024-02-08 20:27:43 -0500

    Add firebase method for authentication

---
+++ firebase/firebase.js
@@ -0,0 +1,29 @@
+import {
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+import {addDoc, collection, getDocs, query, where} from 'firebase/firestore';
+import {auth, db} from './firebaseConfig';
+
+// Auth firebase
+export const authFirebase = async userState => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      userState.email,
+      userState.password,
+    );
+    const userId = userCredential.user.uid;
+    const docRef = query(collection(db, 'users'), where('id', '==', userId));
+    const docSnap = await getDocs(docRef);
+    if (!docSnap.empty) {
+      docSnap.forEach(async doc => {
+        const user = doc.data();
+      });
+      await AsyncStorage.setItem('userId', userId);
+      return true;
+    }
+    return false;
+  } catch (error) {
+    return false;
+  }
+};
