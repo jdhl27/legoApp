403e6030d614e9968f5f470ba7ad45abf71a3a0d
jdhl27 <independientemedellin.27.03@gmail.com>
2024-02-09 07:17:33 -0500

    Modify counter styles

--- src/components/counter/index.js
+++ src/components/counter/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import {sharedStyles} from '../../../constants/sharedStyles';
+import {useUser} from '../../context/user/UserProvider';
+import {getCurrentTheme} from '../../../constants/themes';
 
 const Counter = ({value, onIncrease, onDecrease}) => {
+  const {isDarkTheme} = useUser();
+  const currentTheme = getCurrentTheme(isDarkTheme);
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={onDecrease} style={styles.button}>
-        <Text>-</Text>
+      <TouchableOpacity
+        onPress={onDecrease}
+        style={styles.button(currentTheme)}>
+        <Text style={styles.icon(currentTheme)}>-</Text>
       </TouchableOpacity>
-      <Text style={styles.value}>{value}</Text>
-      <TouchableOpacity onPress={onIncrease} style={styles.button}>
-        <Text>+</Text>
+      <Text style={styles.value(currentTheme)}>{value}</Text>
+      <TouchableOpacity
+        onPress={onIncrease}
+        style={styles.button(currentTheme)}>
+        <Text style={styles.icon(currentTheme)}>+</Text>
       </TouchableOpacity>
     </View>
   );
@@ -20,17 +30,23 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
   },
-  button: {
-    paddingHorizontal: 10,
-    paddingVertical: 5,
+  button: currentTheme => ({
+    paddingHorizontal: 8,
     borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 4,
+    borderColor: currentTheme?.accent,
+    backgroundColor: currentTheme?.accent,
+    borderRadius: 10,
     marginHorizontal: 5,
-  },
-  value: {
+  }),
+  value: currentTheme => ({
     marginHorizontal: 10,
-  },
+    color: currentTheme?.text,
+  }),
+  icon: currentTheme => ({
+    ...sharedStyles.textMedium(currentTheme),
+    fontSize: 26,
+    color: currentTheme?.secondaryText,
+  }),
 });
 
 export default Counter;
