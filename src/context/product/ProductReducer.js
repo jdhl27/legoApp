580692711b7345280e551c5f1ef10753227e0c0c
jdhl27 <independientemedellin.27.03@gmail.com>
2024-02-08 22:11:29 -0500

    Add context reducer to product

---
+++ src/context/product/ProductReducer.js
@@ -0,0 +1,20 @@
+// ProductReducer.js
+import {ADD_PRODUCTS, UPDATE_PRODUCT_QUANTITY} from '../types';
+
+export default (state, action) => {
+  const {type} = action;
+
+  switch (type) {
+    case ADD_PRODUCTS:
+      return [...action.payload];
+
+    case UPDATE_PRODUCT_QUANTITY:
+      const {id, quantity} = action.payload;
+      return state.map(item =>
+        item.id === id ? {...item, stock: quantity} : item,
+      );
+
+    default:
+      return state;
+  }
+};
